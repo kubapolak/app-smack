c1d3379effa1152c77cfb979e0e493a5eb5084e3
Mac <mac@pc11.home>
2017-09-23 18:22:48 +0200

    login VC UI

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Mac on 9/23/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Mac on 9/23/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
