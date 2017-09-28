218b4ea52fac0329ee6d494fbe0b54baef6a162f
Mac <mac@pc11.home>
2017-09-28 19:16:37 +0200

    sockets started

---
+++ smack/smack/Services/SocketService.swift
@@ -0,0 +1,47 @@
+//
+//  SocketService.swift
+//  smack
+//
+//  Created by Mac on 9/28/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    override init() {
+        super.init()
+    }
+    
+    var socket: SocketIOClient = SocketIOClient(socketURL: URL(string: BASE_URL)!)
+    
+    func establishConnetion() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+    
+}
