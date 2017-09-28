b71a40d3df89792182c923b1e83b801a71b86927
Mac <mac@pc11.home>
2017-09-28 17:47:17 +0200

    loading channels

---
+++ smack/smack/Model/Channel.swift
@@ -0,0 +1,24 @@
+//
+//  Channel.swift
+//  smack
+//
+//  Created by Mac on 9/28/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+//    : Decodable
+    
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+    //New swift4 parsing <-> MessageService.swift
+//    public private(set) var _id: String!
+//    public private(set) var name: String!
+//    public private(set) var description: String!
+//    public private(set) var __v: Int?
+    
+}
