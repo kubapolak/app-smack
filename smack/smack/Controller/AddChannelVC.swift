20ce82f34e060e0942277865a335b225478489c8
Mac <mac@pc11.home>
2017-09-28 18:39:42 +0200

    add channel UI

---
+++ smack/smack/Controller/AddChannelVC.swift
@@ -0,0 +1,43 @@
+//
+//  AddChannelVC.swift
+//  smack
+//
+//  Created by Mac on 9/28/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class AddChannelVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var chanDesc: UITextField!
+    
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        
+    }
+    
+    func setupView() {
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(AddChannelVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+        nameTxt.attributedPlaceholder = NSAttributedString(string: "name", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
+        chanDesc.attributedPlaceholder = NSAttributedString(string: "description", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceHolder])
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func createChannelPressed(_ sender: Any) {
+    }
+}
